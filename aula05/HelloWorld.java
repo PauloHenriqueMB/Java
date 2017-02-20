d78108354ee936b57a70d229f7ea60545fb0d96d
Paulo Barbosa <p.h.moreirabarbosa@hotmail.com>
2017-02-20 13:27:05 -0300

    Aula 5 + 6
    https://youtu.be/mu2ti43cgwc
    https://youtu.be/dhGPGv7XCM8

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,9 @@
+//CamelCase é a denominação em inglês para a prática de escrever palavras compostas ou frases, onde cada palavra é iniciada com maiúsculas e unidas sem espaços.
+//http://migre.me/w5YHe
+class HelloWorld{
+
+	
+	public static void main(String []args){
+		System.out.println("Hello World Java Agora vai !!!");
+	}
+}
---
+++ aula06/Argumentos.java
@@ -0,0 +1,6 @@
+class Argumentos{
+
+	public static void main(String []args){
+		System.out.println("Voce Digitou " + args[0]);
+	}
+}
